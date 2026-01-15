8b94615ee9f5e803e4f67c363f6a93120a2f4e30
Павел <mihailov.pashka@yandex.ru>
2026-01-15 15:19:02 +0300

    Added Kafka producer and other relates changes

--- src/main/java/org/wa/google/fit/integration/service/service/impl/GoogleOAuthServiceImpl.java
+++ src/main/java/org/wa/google/fit/integration/service/service/impl/GoogleOAuthServiceImpl.java
@@ -5,7 +5,9 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.wa.google.fit.integration.service.constants.OAuthConstants;
+import org.wa.google.fit.integration.service.dto.GoogleRefreshTokenEvent;
 import org.wa.google.fit.integration.service.exception.ParseTokenException;
 import org.wa.google.fit.integration.service.exception.TokenNotFoundException;
 import org.wa.google.fit.integration.service.service.GoogleOAuthService;
@@ -19,12 +21,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-
+import java.time.Instant;
+import java.time.ZoneOffset;
 import java.util.Base64;
 import java.util.Map;
 
-@Slf4j
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class GoogleOAuthServiceImpl implements GoogleOAuthService {
     @Value("${google-fit.googleapis-base-url}")
@@ -42,8 +45,12 @@ public class GoogleOAuthServiceImpl implements GoogleOAuthService {
     @Value("${GOOGLE_CLIENT_SECRET}")
     private String clientSecret;
 
+    @Value("${kafka.topics.google-refresh-token}")
+    private String googleRefreshTokenTopic;
+
     private final InMemoryTokenStorageService tokenStorage;
     private final OAuthConstants constants;
+    private final KafkaTemplate<String, GoogleRefreshTokenEvent> kafkaTemplate;
 
     private WebClient webClient;
 
@@ -71,29 +78,17 @@ public class GoogleOAuthServiceImpl implements GoogleOAuthService {
                 })
                 .map(tokens -> {
                     String refreshToken = tokens.get("refresh_token");
-                    String idToken = tokens.get("id_token");
-                    String email = extractEmailFromIdToken(idToken);
+                    String email = extractEmailFromIdToken(tokens.get("id_token"));
                     if (email != null && refreshToken != null) {
                         tokenStorage.saveToken(email, refreshToken);
+
+                        log.debug("Sending google refresh token to kafka");
+                        sendRefreshTokenToKafka(email, tokens);
+                    } else {
+                        log.warn("No refresh token in response for email: {}", email);
                     }
                     return tokens;
                 });
-    }
-
-    private String extractEmailFromIdToken(String idToken) {
-        try {
-            String[] parts = idToken.split("\\.");
-            if (parts.length < 2) throw new IllegalArgumentException("Invalid id_token");
-
-            String payloadJson = new String(Base64.getUrlDecoder().decode(parts[1]));
-
-            ObjectMapper mapper = new ObjectMapper();
-            JsonNode payload = mapper.readTree(payloadJson);
-
-            return payload.get("email").asText();
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to parse id_token", e);
-        }
     }
 
     public Mono<Map<String, String>> refreshAccessToken(String email) {
@@ -122,6 +117,44 @@ public class GoogleOAuthServiceImpl implements GoogleOAuthService {
             });
         } catch (Exception e) {
             throw new ParseTokenException("Не удалось прочитать токен");
+        }
+    }
+
+    private void sendRefreshTokenToKafka(String email, Map<String, String> tokens) {
+        try {
+            GoogleRefreshTokenEvent event = new GoogleRefreshTokenEvent(
+                    email,
+                    tokens.get("refresh_token"),
+                    Instant.now().atOffset(ZoneOffset.UTC),
+                    Integer.parseInt(tokens.get("expires_in"))
+            );
+
+            kafkaTemplate.send(googleRefreshTokenTopic, email, event)
+                    .whenComplete((result, ex) -> {
+                        if (ex == null) {
+                            log.info("Google refresh token sent to Kafka for user: {}", email);
+                        } else {
+                            log.error("Failed to send refresh token");
+                        }
+                    });
+        } catch (Exception e) {
+            log.error("Failed to create or send Kafka event", e);
+        }
+    }
+
+    private String extractEmailFromIdToken(String idToken) {
+        try {
+            String[] parts = idToken.split("\\.");
+            if (parts.length < 2) throw new IllegalArgumentException("Invalid id_token");
+
+            String payloadJson = new String(Base64.getUrlDecoder().decode(parts[1]));
+
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode payload = mapper.readTree(payloadJson);
+
+            return payload.get("email").asText();
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to parse id_token", e);
         }
     }
 }
