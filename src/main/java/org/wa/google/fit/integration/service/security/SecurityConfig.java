275d7de261b86e9ebd645d20f5c7ba2757a22364
Павел <mihailov.pashka@yandex.ru>
2026-01-30 03:06:09 +0300

    Changed Security Configuration for device-sync-service

--- src/main/java/org/wa/google/fit/integration/service/security/SecurityConfig.java
+++ src/main/java/org/wa/google/fit/integration/service/security/SecurityConfig.java
@@ -30,6 +30,9 @@ public class SecurityConfig {
                 .authorizeExchange(ex -> ex
                         .pathMatchers(
                                 "/v1/oauth/**",
+                                "v1/google-fit/activity",
+                                "v1/google-fit/sleep",
+                                "v1/google-fit/heart-rate",
                                 "/swagger-ui.html",
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
