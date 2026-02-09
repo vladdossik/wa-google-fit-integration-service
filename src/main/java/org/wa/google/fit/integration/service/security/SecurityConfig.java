04b801e5d0fb2dfceb5aa66c0ab00da6809363f5
Павел <mihailov.pashka@yandex.ru>
2026-02-09 21:35:35 +0300

    Fixed SecurityConfig

--- src/main/java/org/wa/google/fit/integration/service/security/SecurityConfig.java
+++ src/main/java/org/wa/google/fit/integration/service/security/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
                 .authorizeExchange(ex -> ex
                         .pathMatchers(
                                 "/v1/oauth/**",
-                                "v1/google-fit/sync/**",
+                                "/internal/v1/google-fit/sync/**",
                                 "/swagger-ui.html",
                                 "/swagger-ui/**",
                                 "/v3/api-docs/**",
